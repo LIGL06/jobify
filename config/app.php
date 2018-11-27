7ae84363c6167a2f25d467c6ee4d49a2663ac9bc
Iván García <luis.garcialuna@outlook.com>
2018-11-27 12:11:56 -0600

    Cloudinary

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        JD\Cloudder\CloudderServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -227,6 +228,7 @@ return [
         'Html' => Collective\Html\HtmlFacade::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Cloudder' => JD\Cloudder\Facades\Cloudder::class,
 
     ],
 
